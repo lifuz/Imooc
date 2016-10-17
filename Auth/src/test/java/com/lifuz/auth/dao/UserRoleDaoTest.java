9fe835ff8f5257661b02fcd851e3a69d2332d0c4
lifuz <lifuzz@163.com>
2016-10-17 10:17:38 +0800

    dao

--- Auth/src/test/java/com/lifuz/auth/dao/UserRoleDaoTest.java
+++ Auth/src/test/java/com/lifuz/auth/dao/UserRoleDaoTest.java
@@ -1,5 +1,6 @@
 package com.lifuz.auth.dao;
 
+import com.lifuz.auth.entity.UserRole;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -7,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -27,32 +30,58 @@ public class UserRoleDaoTest {
     @Test
     public void saveUserRole() throws Exception {
 
+        UserRole userRole = new UserRole();
+        userRole.setUserId(1000L);
+        userRole.setRoleId(1000L);
 
+        int result = userRoleDao.saveUserRole(userRole);
+
+        logger.debug("result = {} \nuserRole = {}",result,userRole);
 
     }
 
     @Test
     public void getUserRole() throws Exception {
 
+        UserRole userRole = userRoleDao.getUserRole(1000L);
+
+        logger.debug("userRole = {}",userRole);
+
     }
 
     @Test
     public void findUserRolesByUserId() throws Exception {
+
+        List<UserRole> userRoles = userRoleDao.findUserRolesByUserId(1000L);
+
+        logger.debug("userRole = {}",userRoles);
 
     }
 
     @Test
     public void findUserRoles() throws Exception {
 
+        List<UserRole> userRoles = userRoleDao.findUserRoles(0,5);
+
+        logger.debug("userRole = {}",userRoles);
+
     }
 
     @Test
     public void findUserRoleByRoleId() throws Exception {
 
+        List<UserRole> userRoles = userRoleDao.findUserRoleByRoleId(1000L);
+
+        logger.debug("userRole = {}",userRoles);
+
     }
 
     @Test
     public void deleteById() throws Exception {
+
+        int result = userRoleDao.deleteById(1000L);
+
+        logger.debug("result = {}",result);
 
     }
 
