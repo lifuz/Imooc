957deff8b628899f85155bd694cd926fbcee4e81
lifuz <lifuzz@163.com>
2016-10-19 10:07:57 +0800

    start Controller

---
+++ Auth/src/main/java/com/lifuz/auth/controller/RoleController.java
@@ -0,0 +1,14 @@
+package com.lifuz.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 作者：李富
+ * 邮箱：lifuzz@163.com
+ * 时间：2016/10/19 10:04
+ */
+@Controller
+@RequestMapping("/role")
+public class RoleController {
+}
